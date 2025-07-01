a01ae1244288c9539e2179b1bcd8b5ca84f82b08
ramirofazio <ramirofazioyt@gmail.com>
2025-07-01 14:47:17 -0300

    add demo page

---
+++ app/demo/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { BackgroundBeams } from "@/components/AnimatedBackgrounds/BackgroundBeams";
+
+export default function PlantillasAvanzadas() {
+  return (
+    <div className="h-screen w-full bg-secondary flex flex-col items-center justify-center antialiased overflow-x-hidden">
+      <BackgroundBeams />
+    </div>
+  );
+}
