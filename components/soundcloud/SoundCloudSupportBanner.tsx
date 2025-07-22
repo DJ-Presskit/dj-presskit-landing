95dade7b66cf1d00ed359ffd7cf88c0c6a40acbf
ramirofazio <ramirofazioyt@gmail.com>
2025-07-22 15:48:48 -0300

    add link

--- components/soundcloud/SoundCloudSupportBanner.tsx
+++ components/soundcloud/SoundCloudSupportBanner.tsx
@@ -3,12 +3,15 @@ import Icon from "../Icons/Icon";
 import LandingImage from "../Images/LandingImage";
 import Text from "../Text/Text";
 import Image from "next/image";
+import Link from "next/link";
 
 const SoundCloudSupportBanner = () => {
   return (
     <section className="bg-gradient-to-t from-secondary via-[#ff5500]/50 to-secondary  w-full py-12 px-6 flex flex-col items-center justify-center text-center gap-5">
       <div className="flex items-center  flex-col">
-        <Icon src="soundcloud" className="w-32 h-32 md:w-32 md:h-32" />
+        <Link href={"https://soundcloud.com"} target="_blank">
+          <Icon src="soundcloud" className="w-32 h-32 md:w-32 md:h-32" />
+        </Link>
         <Text
           Tag={"h5"}
           variant="title"
